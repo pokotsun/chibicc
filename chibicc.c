92a488d9d24b6bbbaf9855cd67b3cc2dabe1dded
pokotsun <gorigolila@gmail.com>
2020-05-02 01:49:44 +0900

    implement simple tokenizer

--- chibicc.c
+++ chibicc.c
@@ -1,5 +1,113 @@
+#include<ctype.h>
+#include<stdarg.h>
+#include<stdbool.h>
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<string.h>
+
+// kinds of token
+typedef enum {
+	TK_RESERVED, // symbol
+	TK_NUM, // number token
+	TK_EOF, // end of input
+} TokenKind;
+
+typedef struct Token Token;
+
+
+// token type
+struct Token {
+	TokenKind kind; // token type
+	Token *next; // next input token
+	int val; // kindがTK_NUMの場合, その数値
+	char *str; // トークン文字列
+};
+
+// current focused token 
+Token *token;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+// 次のトークンが期待している記号の時には, トークンを1つ読み進めて
+// 真を返す. それ以外の場合には偽を返す.
+bool consume(char op) {
+	if(token->kind != TK_RESERVED || token->str[0] != op) {
+		return false;
+	}
+	token = token->next;
+	return true;
+}
+
+// 次のトークンが期待している記号の時には, トークンを1つ読み進める
+// それ以外の場合にはエラーを返す.
+void expect(char op) {
+	if(token->kind != TK_RESERVED || token->str[0] != op) {
+		error("not '%c'.", op);
+	}
+	token = token->next;
+}
+
+// 次のトークンが数値の場合, トークンを1つ読み進めてその数値を返す.
+// それ以外の場合にはエラーを報告する.
+int expect_number() {
+	if(token->kind != TK_NUM) {
+		error("Not Number.");
+	}
+	int val = token->val;
+	token = token->next;
+	return val;
+}
+
+bool at_eof() {
+	return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->str = str;
+	cur->next = tok;
+	return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+	Token head;
+	head.next = NULL;
+	Token *cur = &head;
+
+	while(*p) {
+		if(isspace(*p)) {
+			p++;
+			continue;
+		}
+
+		if(*p == '+' || *p == '-') {
+			cur = new_token(TK_RESERVED, cur, p++);
+			continue;
+		}
+
+		if(isdigit(*p)) {
+			cur = new_token(TK_NUM, cur, p);
+			cur->val = strtol(p, &p, 10);
+			continue;
+		}
+
+		error("can not tokenize.");
+	}
+
+	new_token(TK_EOF, cur, p);
+	return head.next;
+}
 
 int main(int argc, char**argv) {
 	if(argc != 2) {
@@ -7,28 +115,28 @@ int main(int argc, char**argv) {
 		return 1;
 	}
 
-	char *p = argv[1];
+	// tokenizer
+	token = tokenize(argv[1]);
 
+	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-	while (*p) {
-		if(*p == '+') {
-			p++;
-			printf("  add rax, %ld\n", strtol(p, &p, 10));
+	// 式の最初は数でなければならないので, それをチェックして
+	// 最初のmov命令を出力
+	printf("  mov rax, %d\n", expect_number());
+
+	// `+ <num>`or `- <num>`というトークンの並びを消費しつつ
+	// アセンブリを出力
+	while (!at_eof()) {
+		if(consume('+')) {
+			printf("  add rax, %d\n", expect_number());
 			continue;
 		}
 
-		if(*p == '-') {
-			p++;
-			printf("  sub rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
-
-		fprintf(stderr, "unexpected character: '%c'\n", *p);
-		return 1;
+		expect('-');
+		printf("  sub rax, %d\n", expect_number());
 	}
 
 	printf("  ret\n");
