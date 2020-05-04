5c5e9ebd03d152bee0d23d864f49ea6f3726b7a0
pokotsun <gorigolila@gmail.com>
2020-05-05 01:15:28 +0900

    impl recursive descent parsing

--- chibicc.c
+++ chibicc.c
@@ -8,6 +8,7 @@
 // input
 char *user_input;
 
+
 // エラー箇所を報告する
 void error_at(char *loc, char *fmt, ...) {
 	va_list ap;
@@ -24,7 +25,7 @@ void error_at(char *loc, char *fmt, ...) {
 
 // kinds of token
 typedef enum {
-	TK_RESERVED, // symbol
+	TK_RESERVED, // 予約語(演算子とか)
 	TK_NUM, // number token
 	TK_EOF, // end of input
 } TokenKind;
@@ -124,6 +125,83 @@ Token *tokenize(char *p) {
 
 	new_token(TK_EOF, cur, p);
 	return head.next;
+}
+
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // number 
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+	NodeKind kind; // ノードの型
+	Node *lhs; // 左辺
+	Node *rhs; // 右辺
+	int val; // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+	Node *node = mul();
+
+	for(;;) {
+		if(consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		} else if(consume('-')) {
+			node = new_node(ND_SUB, node, mul());
+		} else {
+			return node;
+		}
+	}
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for(;;) {
+		if(consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		} else if(consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		} else {
+			return node;
+		}
+	}
+}
+
+Node *primary() {
+	// 次のトークンが"("なら, "(" expr ")"のはず
+	if(consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
 }
 
 int main(int argc, char**argv) {
