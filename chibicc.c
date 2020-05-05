1da8d3edd31ffa62a82cf38fad21a11d48853483
pokotsun <gorigolila@gmail.com>
2020-05-05 13:44:59 +0900

    implement ==, !=, <= and >= operators

--- chibicc.c
+++ chibicc.c
@@ -39,6 +39,7 @@ struct Token {
 	Token *next; // next input token
 	int val; // kindがTK_NUMの場合, その数値
 	char *str; // トークン文字列
+	int len; // トークンの長さ
 };
 
 // current focused token 
@@ -56,8 +57,10 @@ void error(char *fmt, ...) {
 
 // 次のトークンが期待している記号の時には, トークンを1つ読み進めて
 // 真を返す. それ以外の場合には偽を返す.
-bool consume(char op) {
-	if(token->kind != TK_RESERVED || token->str[0] != op) {
+bool consume(char *op) {
+	if(token->kind != TK_RESERVED ||
+		strlen(op) != token->len ||
+		memcmp(token->str, op, token->len)) {
 		return false;
 	}
 	token = token->next;
@@ -66,8 +69,10 @@ bool consume(char op) {
 
 // 次のトークンが期待している記号の時には, トークンを1つ読み進める
 // それ以外の場合にはエラーを返す.
-void expect(char op) {
-	if(token->kind != TK_RESERVED || token->str[0] != op) {
+void expect(char *op) {
+	if(token->kind != TK_RESERVED ||
+		strlen(op) != token->len ||
+		memcmp(token->str, op, token->len)) {
 		error_at(token->str, "not '%c'.", op);
 	}
 	token = token->next;
@@ -89,12 +94,17 @@ bool at_eof() {
 }
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
 	tok->str = str;
+	tok->len = len;
 	cur->next = tok;
 	return tok;
+}
+
+bool startswitch(char *p, char *q) {
+	return memcmp(p, q, strlen(q)) == 0;
 }
 
 // 入力文字列pをトークナイズしてそれを返す
@@ -109,23 +119,39 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		if( *p == '+' || *p == '-' || 
-			*p == '*' || *p == '/' || 
-			*p == '(' || *p == ')') {
-			cur = new_token(TK_RESERVED, cur, p++);
+		// Multi-letter punctuator
+		if(startswitch(p, "==") || startswitch(p, "!=") ||
+		   startswitch(p, "<=") || startswitch(p, ">=")) {
+			cur = new_token(TK_RESERVED, cur, p, 2);
+			p += 2;
 			continue;
 		}
 
+		// Single-letter punctuator
+		if (strchr("+-*/()<>", *p)) {
+			cur = new_token(TK_RESERVED, cur, p++, 1);
+			continue;
+		}
+		// if( *p == '+' || *p == '-' || 
+		// 	*p == '*' || *p == '/' || 
+		// 	*p == '(' || *p == ')') {
+		// 	cur = new_token(TK_RESERVED, cur, p++);
+		// 	continue;
+		// }
+
+		// Integer literal
 		if(isdigit(*p)) {
-			cur = new_token(TK_NUM, cur, p);
+			cur = new_token(TK_NUM, cur, p, 0);
+			char *q = p;
 			cur->val = strtol(p, &p, 10);
+			cur->len = p - q;
 			continue;
 		}
 
 		error_at(cur->str, "can not tokenize.");
 	}
 
-	new_token(TK_EOF, cur, p);
+	new_token(TK_EOF, cur, p, 0);
 	return head.next;
 }
 
@@ -135,6 +161,10 @@ typedef enum {
 	ND_SUB, // -
 	ND_MUL, // *
 	ND_DIV, // /
+	ND_EQ,  // ==
+	ND_NE,  // !=
+	ND_LT,  // <
+	ND_LE,  // <=
 	ND_NUM, // number 
 } NodeKind;
 
@@ -164,17 +194,71 @@ Node *new_node_num(int val) {
 }
 
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *mul();
 Node *primary();
 Node *unary();
 
+// expr = equality
 Node *expr() {
+	return equality();
+	// Node *node = mul();
+
+	// while(true) {
+	// 	if(consume('+')) {
+	// 		node = new_node(ND_ADD, node, mul());
+	// 	} else if(consume('-')) {
+	// 		node = new_node(ND_SUB, node, mul());
+	// 	} else {
+	// 		return node;
+	// 	}
+	// }
+}
+
+// equality = relational ("==" relational | "!=" relational)*
+Node *equality() {
+	Node *node = relational();
+
+	while(true) {
+		if (consume("==")) {
+			node = new_node(ND_EQ, node, relational());
+		} else if(consume("!=")) {
+			node = new_node(ND_NE, node, relational());
+		} else {
+			return node;
+		}
+	}
+}
+
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+Node *relational() {
+	Node *node = add();
+
+	while(true) {
+		if(consume("<")) {
+			node = new_node(ND_LT, node, add());
+		} else if(consume("<=")) {
+			node = new_node(ND_LE, node, add());
+		} else if(consume(">")) {
+			node = new_node(ND_LT, add(), node);
+		} else if(consume(">=")) {
+			node = new_node(ND_LE, add(), node);
+		} else {
+			return node;
+		}
+	}
+}
+
+// add = mul ("+" mul | "-" mul)*
+Node *add() {
 	Node *node = mul();
 
 	while(true) {
-		if(consume('+')) {
+		if(consume("+")) {
 			node = new_node(ND_ADD, node, mul());
-		} else if(consume('-')) {
+		} else if(consume("-")) {
 			node = new_node(ND_SUB, node, mul());
 		} else {
 			return node;
@@ -182,40 +266,45 @@ Node *expr() {
 	}
 }
 
+// mul = unary("*" unary | "/" unary)*
 Node *mul() {
 	Node *node = unary();
 
 	while(true) {
-		if(consume('*')) {
-			node = new_node(ND_MUL, node, primary());
-		} else if(consume('/')) {
-			node = new_node(ND_DIV, node, primary());
+		if(consume("*")) {
+			node = new_node(ND_MUL, node, unary());
+		} else if(consume("/")) {
+			node = new_node(ND_DIV, node, unary());
 		} else {
 			return node;
 		}
 	}
 }
 
+// unary = ("+" | "-")? unary
+//			| primary
+Node *unary() {
+	if(consume("+")) {
+		return unary();
+	}
+	else if(consume("-")) {
+		return new_node(ND_SUB, new_node_num(0), unary());
+	}
+	return primary();
+}
+
+// primary = "(" expr ")" | num
 Node *primary() {
 	// 次のトークンが"("なら, "(" expr ")"のはず
-	if(consume('(')) {
+	if(consume("(")) {
 		Node *node = expr();
-		expect(')');
+		expect(")");
 		return node;
 	}
 
 	return new_node_num(expect_number());
 }
 
-Node *unary() {
-	if(consume('+')) {
-		return primary();
-	}
-	else if(consume('-')) {
-		return new_node(ND_SUB, new_node_num(0), primary());
-	}
-	return primary();
-}
 
 void gen(Node *node) {
 	if(node->kind == ND_NUM) {
@@ -242,6 +331,26 @@ void gen(Node *node) {
 		case ND_DIV:
 			printf("  cqo\n");
 			printf("  idiv rdi\n");
+			break;
+		case ND_EQ:
+			printf("  cmp rax, rdi\n");
+			printf("  sete al\n");
+			printf("  movzb rax, al\n");
+			break;
+		case ND_NE:
+			printf("  cmp rax, rdi\n");
+			printf("  setne al\n");
+			printf("  movzb rax, al\n");
+			break;
+		case ND_LT:
+			printf("  cmp rax, rdi\n");
+			printf("  setl al\n");
+			printf("  movzb rax, al\n");
+			break;
+		case ND_LE:
+			printf("  cmp rax, rdi\n");
+			printf("  setle al\n");
+			printf("  movzb rax, al\n");
 			break;
 	}
 
--- test.sh
+++ test.sh
@@ -22,7 +22,8 @@ assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
 assert 47 "5+6*7" 
 assert 15 "5*(9-6)"
-assert 4 "(3+5)/2"
+assert  4 "(3+5)/2"
+assert 12 "-4 + +16"
 assert 6 "-4 + 10"
 
 echo OK
