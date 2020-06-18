e7c0da826730b942d8588513902a2337fb63e771
pokotsun <gorigolila@gmail.com>
2020-06-18 22:25:44 +0900

    add char literal

--- tokenize.c
+++ tokenize.c
@@ -163,6 +163,30 @@ static Token *read_string_literal(Token *cur, char *start) {
     return tok;
 }
 
+static Token *read_char_literal(Token *cur, char *start) {
+    char *p = start + 1;
+    if(*p == '\0') {
+        error_at(start, "unclosed char literal");
+    }
+
+    char c;
+    if(*p == '\\') {
+        p++;
+        c = get_escape_char(*p++);
+    } else {
+        c = *p++;
+    }
+
+    if(*p != '\'') {
+        error_at(start, "char literal too long");
+    }
+    p++;
+
+    Token *tok = new_token(TK_NUM, cur, start, p - start);
+    tok->val = c;
+    return tok; 
+}
+
 // 入力文字列pをトークナイズしてそれを返す
 Token *tokenize() {
 	char *p = user_input;
@@ -199,6 +223,13 @@ Token *tokenize() {
         // String literal
         if(*p == '"') {
             cur = read_string_literal(cur, p);
+            p += cur->len;
+            continue;
+        }
+
+        // Character literal
+        if(*p == '\'') {
+            cur = read_char_literal(cur, p);
             p += cur->len;
             continue;
         }
